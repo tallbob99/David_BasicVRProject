a7a6468a6d3cbadccc70e1ed9427b874cf8ce3f5
tallbob99 <davidm1ller@shaw.ca>
2020-11-12 15:27:16 -0700

    raycast script started

---
+++ Assets/Scripts/OpenDoorStudent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorStudent : MonoBehaviour
+{
+    public bool unlocked = false;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public float doorOpenSpeed = 1.5f;
+    public GameObject door;
+
+    private void Start()
+    {
+        // sets the starting and open position of the door
+        startPos = door.transform.position;
+        endPos = new Vector3(door.transform.position.x - 3.5f, door.transform.position.y, door.transform.position.z);
+    }
+
+    IEnumerator MoveDoor(Vector3 start, Vector3 end)
+    {
+        float elapsedTime = 0f;
+        // while the elapsed time is less than 1.5, keep moving the door
+        // if you want the door to open faster/slower, change the 1.5f value
+        while (elapsedTime < doorOpenSpeed)
+        {
+            // move the door smoothly using Lerp
+            // learn more about Lerp here: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+            door.transform.position = Vector3.Lerp(start, end, (elapsedTime / doorOpenSpeed));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1f)) 
+        {
+
+            Debug.Log("hit");
+        }
+
+    }
+}

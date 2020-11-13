f0e0cc163af4e7f63b79636c5cb2de6b416c6715
tallbob99 <davidm1ller@shaw.ca>
2020-11-12 18:31:16 -0700

    menu button prototype works

---
+++ Assets/Scripts/buttonPress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonPress : MonoBehaviour
+{
+    
+    public void ButtonToggle()
+    {
+        Debug.Log("button has been pressed");
+
+    }
+
+}

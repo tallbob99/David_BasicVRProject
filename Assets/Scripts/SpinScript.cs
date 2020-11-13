1e23a7383d747486ef36871f8c7ad0500209327b
tallbob99 <davidm1ller@shaw.ca>
2020-11-12 17:25:24 -0700

    object rotates with trigger push

--- Assets/Scripts/SpinScript.cs
+++ Assets/Scripts/SpinScript.cs
@@ -6,6 +6,9 @@ using Valve.VR;
 public class SpinScript : MonoBehaviour
 {
     public SteamVR_Action_Single spinAction;
+    public float rotateSpeed;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,10 @@ public class SpinScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (spinAction.axis > 0)
+        {
+            transform.Rotate(0, spinAction.axis * rotateSpeed, 0, Space.Self);
+        }
         
     }
 }
